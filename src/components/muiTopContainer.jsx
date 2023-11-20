7387623a858e1f7c044710d97ad67c27e1f9d4fb
Your Name~ <fixchaim@gmail.com>
2023-11-20 15:11:32 +0200

    Data_from_JSON_and_disply

--- src/components/muiTopContainer.jsx
+++ src/components/muiTopContainer.jsx
@@ -1,45 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import { Box } from '@mui/material';
-import Filter from './FilterDropDown';
-import WelcomeTextComponent from './WelcomeTextComponent';
-import { FilterSprint } from './SprintFilter';
+import React, { useState, useEffect } from "react";
+import { Box } from "@mui/material";
+import Filter from "./FilterDropDown";
+import WelcomeTextComponent from "./WelcomeTextComponent";
+import { FilterSprint } from "./SprintFilter";
+import data from "./mockData/data";
 
 const MainComponentMui = () => {
+  // Extract unique values for each property
+  const uniqueAssignee = Array.from(new Set(data.map((item) => item.userName)));
+  const uniqueIssues = Array.from(new Set(data.map((item) => item.issue)));
+  const uniqueCategories = Array.from(new Set(data.map((item) => item.category)));
+  const uniqueMileStones = Array.from(new Set(data.map((item) => item.mileStone)));
+  const uniqueProjects = Array.from(new Set(data.map((item) => item.project)));
+  const uniqueSprints = Array.from(new Set(data.map((item) => item.sprint)));
 
-  const [selectedOption1, setSelectedOption1] = useState('');
-  const [selectedOption2, setSelectedOption2] = useState('');
-  const [selectedOption3, setSelectedOption3] = useState('');
-  const [selectedOption4, setSelectedOption4] = useState('');
+  console.log("Unique Assignee:", uniqueAssignee);
+  console.log("Unique Issues:", uniqueIssues);
+  console.log("Unique Categories:", uniqueCategories);
+  console.log("Unique MileStones:", uniqueMileStones);
+  console.log("Unique Projects:", uniqueProjects);
+  console.log("Unique Sprints:", uniqueSprints);
 
-  const options1 = ['Option A', 'Option B', 'Option C'];
-  const options2 = ['Option X', 'Option Y', 'Option Z'];
-  const options3 = ['Choice 1', 'Choice 2', 'Choice 3'];
-  const options4 = ['Category 1', 'Category 2', 'Category 3'];
+  const [selectedIssue, setSelectedIssue] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedMileStones, setSelectedMileStones] = useState("");
+  const [selectedAssignee, setSelectedAssignee] = useState("");
 
-  
-
+  const issue = uniqueIssues;
+  const Category = uniqueCategories;
+  const MileStones = uniqueMileStones;
+  const Assignee = uniqueAssignee;
 
   return (
     <Box
-    sx={{
-      margin: '20px',
-      display: 'flex',
-      flexDirection: 'column',
-      color: '#ffffff',
+      sx={{
+        margin: "20px",
+        display: "flex",
+        flexDirection: "column",
+        color: "#ffffff",
       }}
     >
       <WelcomeTextComponent />
       <FilterSprint />
-  
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', marginBottom: '20px' }}>
-      <Filter label="Issue Type" options={options1} selectedOption={selectedOption1} onChange={setSelectedOption1} />
-      <Filter label="Category" options={options2} selectedOption={selectedOption2} onChange={setSelectedOption2} />
-      <Filter label="Milestone" options={options3} selectedOption={selectedOption3} onChange={setSelectedOption3} />
-      <Filter label="Assignee" options={options4} selectedOption={selectedOption4} onChange={setSelectedOption4} />
+
+      <Box sx={{ display: "flex", flexWrap: "wrap", marginBottom: "20px" }}>
+        <Filter
+          label="Issue Type"
+          options={issue}
+          selectedOption={selectedIssue}
+          onChange={setSelectedIssue}
+        />
+        <Filter
+          label="Category"
+          options={Category}
+          selectedOption={selectedCategory}
+          onChange={setSelectedCategory}
+        />
+        <Filter
+          label="Milestone"
+          options={MileStones}
+          selectedOption={selectedMileStones}
+          onChange={setSelectedMileStones}
+        />
+        <Filter
+          label="Assignee"
+          options={Assignee}
+          selectedOption={selectedAssignee}
+          onChange={setSelectedAssignee}
+        />
       </Box>
     </Box>
   );
 };
-
 
 export default MainComponentMui;
