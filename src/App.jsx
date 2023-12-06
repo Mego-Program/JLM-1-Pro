85806b49f1de943fbc95a6c4382cd221075c9fc2
Your Name <you@example.com>
2023-12-06 13:03:34 +0200

    i add my component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import KanbanBoard from "./components/KanbanBoard";
 
 import SpecButton from "./components/ButtonSpecs";
+import YourComponent from "./components/Settings";
 
 export const RouterPro = (
   <>
     <SpecButton />
     <KanbanBoard />
+    <YourComponent/>
   </>
 );
 
