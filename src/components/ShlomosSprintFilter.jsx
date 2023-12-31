f7af30e37f0f648ae8c256ca1af3d5815230864b
Shlomo Auerbach <shlomoa40@gmail.com>
2023-12-31 11:36:54 +0200

    delete app.jsx and edit sprint

--- src/components/ShlomosSprintFilter.jsx
+++ src/components/ShlomosSprintFilter.jsx
@@ -1,114 +1,50 @@
-// import * as React from 'react';
-// import PropTypes from 'prop-types';
-// import { Select as BaseSelect, selectClasses } from '@mui/base/Select';
-// import { Option as BaseOption, optionClasses } from '@mui/base/Option';
-// import { Popper as BasePopper } from '@mui/base/Popper';
-// import { styled } from '@mui/system';
-// import UnfoldMoreRoundedIcon from '@mui/icons-material/UnfoldMoreRounded';
-// import axios from 'axios';
-
-
-
-// const Select = React.forwardRef(function Select(props, ref) {
-//   const slots = {
-//     root: CustomButton,
-//     listbox: Listbox,
-//     popper: Popper,
-//     ...props.slots,
-//   };
-
-//   return <BaseSelect {...props} ref={ref} slots={slots} />;
-// });
-
-
-// export async function getAllSprints(){
-//     try {
-//         const response = await axios.post('http://localhost:8137/projects/getAllSprints')
-//         console.log(response.data)
-//         return response.data
-//       } catch (error) {
-//         console.error('Error fetching tasks:', error.message);
-//         return null
-//       }
-//       console.log(response.data);
-// }
-
-// const sprints = getAllSprints();
-
-// // const selectSprint = async () => {
-// //   // TODO: Implement logic to send data to the server
-// //   try {
-// //     const response = await axios.post("/add_sprint",sprintData)
-// //   } 
-// //   catch (error) {  
-// //   };
-// //   console.log("sprint data:", sprintData);
-// //   // Close the modal after creating the sprint
-// //   handleClose();
-// // };
-
-
-
-// export default function ShlomosSprintFilter() {
-//   const [sprints, setSprints] = useState([]);
-//   return (
-//     <Select placeholder='Select a sprint'>
-//      {sprints.sprintName}
-//     </Select>
-//   );
-// }
-
-
-
-
-
 // import React, { useState, useEffect } from 'react';
-// import {Select, menuItem} from '@mui/material';
+// import { Select, MenuItem } from '@mui/material';
 // import axios from 'axios';
 
-// export async function getAllSprints() {
-//   try {
-//     const response = await axios.post('http://localhost:8137/projects/GetAllSprints');
-//     console.log(response.data);
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error fetching sprints:', error.message);
-//     return null;
-//   }
-// }
-
-// export default function ShlomosSprintFilter() {
+// export default function ShlomosSprintFilter(sprintData) {
+//   const [selectedSprint, setSelectedSprint] = useState('');
 //   const [sprints, setSprints] = useState([]);
 
 //   useEffect(() => {
 //     const fetchData = async () => {
-//       const fetchedSprints = await getAllSprints();
-//       if (fetchedSprints) {
-//         setSprints(fetchedSprints);
+//       try {
+//         const response = await axios.post('http://localhost:8137/projects/GetAllSprints');
+//         const sprintData = response.data.map((project) => project.Sprint.map((sprint) => sprint.sprintName)).flat();
+//         setSprints(sprintData);
+//         console.log('response.data:', response.data);
+//         console.log('sprintdata:',sprintData);
+//       } catch (error) {
+//         console.error('Error fetching sprints:', error.message);
 //       }
 //     };
 
 //     fetchData();
-//   }, []);
+//   }, []); // Empty dependency array ensures it runs only once on mount
+
+//   const handleChange = (event) => {
+//     setSelectedSprint(event.target.value);
+//   };
 
 //   return (
-//     <Select placeholder='Select a sprint'>
-//       {sprints.map((sprint) => (
-//         <Select key={sprint._id} value={sprint}>
-//           {sprint.Sprint}
-//         </Select>
+//     <Select value={selectedSprint} onChange={handleChange}>
+//       <MenuItem value="" disabled>Select a sprint</MenuItem>
+//       {sprints.map((sprintData) => (
+//         <MenuItem key={sprintData} value={sprintData}>
+//           {sprintData}
+//           {console.log('iv opened')};
+//         </MenuItem>
 //       ))}
 //     </Select>
 //   );
 // }
 
 
-
 import React, { useState, useEffect } from 'react';
-import { Select, MenuItem } from '@mui/material';
+import { Select, MenuItem, Button } from '@mui/material';
 import axios from 'axios';
 
-export default function ShlomosSprintFilter(sprintData) {
+export default function ShlomosSprintFilter() {
   const [selectedSprint, setSelectedSprint] = useState('');
   const [sprints, setSprints] = useState([]);
 
@@ -116,31 +52,50 @@ export default function ShlomosSprintFilter(sprintData) {
     const fetchData = async () => {
       try {
         const response = await axios.post('http://localhost:8137/projects/GetAllSprints');
-        const sprintData = response.data.map((project) => project.Sprint.map((sprint) => sprint.sprintName)).flat();
-        // setSprints(sprintData);
-        console.log('response.data:', response.data);
-        console.log('sprintdata:',sprintData);
+        const sprintData = response.data.map((project) => project.Sprint.map((sprint) => sprint)).flat();
+        setSprints(sprintData);
       } catch (error) {
         console.error('Error fetching sprints:', error.message);
       }
     };
 
     fetchData();
-  }, []); // Empty dependency array ensures it runs only once on mount
+  }, []); 
 
   const handleChange = (event) => {
     setSelectedSprint(event.target.value);
   };
 
+  const handleEdit = (sprintId) => {
+    // Implement logic to handle edit operation for the specific sprint
+    console.log(`Editing sprint with ID: ${sprintId}`);
+  };
+
+  const handleDelete = (sprintId) => {
+    // Implement logic to handle delete operation for the specific sprint
+    console.log(`Deleting sprint with ID: ${sprintId}`);
+  };
+
   return (
-    <Select value={selectedSprint} onChange={handleChange}>
-      <MenuItem value="" disabled>Select a sprint</MenuItem>
-      {sprints.map((sprintData) => (
-        <MenuItem key={sprintData} value={sprintData}>
-          {sprintData}
-          {console.log('iv opened')};
-        </MenuItem>
-      ))}
-    </Select>
+    <div>
+      <Select value={selectedSprint} onChange={handleChange}>
+        <MenuItem value="" disabled>Select a sprint</MenuItem>
+        {sprints.map((sprint) => (
+          <div key={sprint._id} style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <MenuItem value={sprint.sprintName}>
+              {sprint.sprintName}
+            </MenuItem>
+            <div>
+              <Button onClick={() => handleEdit(sprint._id)} variant="outlined" color="primary">
+                Edit
+              </Button>
+              <Button onClick={() => handleDelete(sprint._id)} variant="outlined" color="secondary">
+                Delete
+              </Button>
+            </div>
+          </div>
+        ))}
+      </Select>
+    </div>
   );
 }
